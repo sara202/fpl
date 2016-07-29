bed943b99713367b92aad558b6ec3bfc1cd08288
sara202 <sara.202@hotmail.co.uk>
2016-07-29 15:27:03 +0100

    renamed app.js to server.js

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
+var http = require('http');
+var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+    port = process.env.OPENSHIFT_NODEJS_PORT || '8080';
+
 
 mongoose.connect('mongodb://testUser:test1@ds023315.mlab.com:23315/footballleaguepredictions');
 var db = mongoose.connection;
