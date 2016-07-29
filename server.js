1de6b6a13546091d91cb0b61214942ea38e07686
sara202 <sara.202@hotmail.co.uk>
2016-07-29 15:45:22 +0100

    server.js amended

--- server.js
+++ server.js
@@ -97,3 +97,7 @@ app.listen(app.get('port'), function(){
 	console.log('Server started on port '+app.get('port'));
 });
 
+
+app.get('/', function (req, res) {
+    res.status('200').send('Service is up');
+});
