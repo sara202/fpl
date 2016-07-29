72487af3746165eb9bf4f59695a5121bd44ac763
sara202 <sara.202@hotmail.co.uk>
2016-07-29 15:02:54 +0100

    changed server.js file

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-var http = require("http");
+var http = require('http');
+var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+    port = process.env.OPENSHIFT_NODEJS_PORT || '8080';
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("It's alive!");
-  response.end();
-}).listen(3000);
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(port, ip);
+console.log('Server running at http://'+ip+':'+port+'/');
+
